4087dcca7cef0ad4e56f2dcc08f64cecac96e4be
Łukasz Wawrzyniak <wowvaqa@gamil.com>
2021-12-12 13:09:16 +0100

    UseReducer tutorial part 1

--- src/App.js
+++ src/App.js
@@ -12,12 +12,13 @@ import React from "react";
 // import ShowHide from "./tutorial/3-conditional-rendering/setup/3-show-hide";
 // import Forms from "./tutorial/4-forms/setup/1-controlled-inputs";
 // import MultipleInputs from "./tutorial/4-forms/setup/2-multiple-inputs";
-import UseRef from "./tutorial/5-useRef/setup/1-useRef-basics";
+// import UseRef from "./tutorial/5-useRef/setup/1-useRef-basics";
+import UseReducer from "./tutorial/6-useReducer/setup/";
 
 function App() {
   return (
     <div className="container">
-      <UseRef></UseRef>
+      <UseReducer></UseReducer>
     </div>
   );
 }
--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,74 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
 // reducer function
 
+/**
+ * Funkcja reducer która jest parametrem 'useReducer' przyjmuje dwa argumenty:
+ * 'state' który jest poczatkowym stanem, orac 'action' który jest akcją
+ * Funkcja 'reducer' musi coś zwrócć
+ */
+
+const reducer = (state, action) => {
+  if (action.type === "ADD_ITEM") {
+    const newPeople = [...state.people, action.payLoad];
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: "item added",
+    };
+  }
+  if (action.type === "NO_VALUE") {
+    return { ...state, isModalOpen: true, modalContent: "Proszę wpisz imię" };
+  }
+  throw new Error("No matching action (dispatch) type");
+};
+
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: "",
+};
+
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name, setName] = useState("");
+  // Przy użyci 'useReducer' domyślna konwencja nazewnictwa to: [state, dispach]
+  // state - początkowy stan; dispatch - wysyłka danych, akcja
+  const [state, dispatch] = useReducer(reducer, defaultState);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (name) {
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({ type: "ADD_ITEM", payLoad: newItem });
+      setName("");
+    } else {
+      dispatch({ type: "NO_VALUE" });
+    }
+  };
+
+  return (
+    <>
+      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+          <button type="submit">Add Person</button>
+        </div>
+      </form>
+      {state.people.map((person) => {
+        return (
+          <div key={person.id}>
+            <h4>{person.name} </h4>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Index;
