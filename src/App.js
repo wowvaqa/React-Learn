13a1e4ee17ec7e31f3f0f79455f3e7b60ccd2c72
Łukasz Wawrzyniak <wowvaqa@gamil.com>
2021-12-16 18:42:07 +0100

    PropTyping project tutorial end

--- src/App.js
+++ src/App.js
@@ -16,12 +16,13 @@ import React from "react";
 // import UseReducer from "./tutorial/6-useReducer/setup/";
 // import PropDrilling from "./tutorial/7-prop-drilling/setup/1-prop-drilling";
 // import Context from "./tutorial/8-useContext/setup/1-context-api";
-import Custom from "./tutorial/9-custom-hooks/setup/1-fetch-example";
+// import Custom from "./tutorial/9-custom-hooks/setup/1-fetch-example";
+import PropTypes from "./tutorial/10-prop-types/setup";
 
 function App() {
   return (
     <div className="container">
-      <Custom></Custom>
+      <PropTypes></PropTypes>
     </div>
   );
 }
